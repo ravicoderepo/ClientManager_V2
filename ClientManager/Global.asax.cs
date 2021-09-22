e80483449dc02a6c7cc6fde2b1b27f7af2068a26
Ravikumar <anbu.ravi04@outlook.com>
2021-09-22 21:21:00 +0530

    Test Commit

--- ClientManager/Global.asax.cs
+++ ClientManager/Global.asax.cs
@@ -11,6 +11,7 @@ namespace ClientManager
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
