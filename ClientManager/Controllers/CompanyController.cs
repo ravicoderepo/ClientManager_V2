028b27d9d5380f76ea635697e5bf5c59933ade66
ravipoc <anbu.ravi04@outlook.com>
2022-09-22 08:37:50 +0530

    Change done

--- ClientManager/Controllers/CompanyController.cs
+++ ClientManager/Controllers/CompanyController.cs
@@ -12,7 +12,7 @@ using DBOperation;
 
 namespace ClientManager.Controllers
 {
-    public class ItemsController : Controller
+    public class CompanyController : Controller
     {
         private ClientManagerEntities db = new ClientManagerEntities();
 
@@ -20,7 +20,7 @@ namespace ClientManager.Controllers
         [CustomAuthorize(new string[] { "Super Admin","Super User" })]
         public ActionResult List()
         {
-            var items = db.Items;
+            var items = db.Companies;
             return View(items.ToList());
         }
 
@@ -28,8 +28,6 @@ namespace ClientManager.Controllers
         [CustomAuthorize(new string[] { "Super Admin", "Super User" })]
         public ActionResult Create()
         {
-            ViewBag.MaterialId = new SelectList(db.Materials, "MaterialId", "MaterialName", 1).ToList<SelectListItem>();
-            ViewBag.TypeId = new SelectList(db.Types, "TypeId", "TypeName", 1).ToList<SelectListItem>();
             ViewBag.Status = new SelectList(Utility.DefaultList.GetStatusList(), "Value", "Text", 1).ToList<SelectListItem>();
             return View();
         }
@@ -39,7 +37,7 @@ namespace ClientManager.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [CustomAuthorize(new string[] { "Super Admin", "Super User" })]
-        public ActionResult Create(Models.ItemData itemData)
+        public ActionResult Create(Models.CompanyData companyData)
         {
             UserDetails userData = (UserDetails)this.Session["UserDetails"];
             
@@ -49,7 +47,7 @@ namespace ClientManager.Controllers
             try
             {
                 int num = 0;
-                if (string.IsNullOrEmpty(itemData.ItemName) || string.IsNullOrEmpty(itemData.Description))
+                if (string.IsNullOrEmpty(companyData.Name) || string.IsNullOrEmpty(companyData.Description))
                 {
                     jsonReponse = new JsonReponse()
                     {
@@ -62,12 +60,9 @@ namespace ClientManager.Controllers
                 {
                     this.db.Items.Add(new DBOperation.Item()
                     {
-                        ItemName = itemData.ItemName,
-                        TypeId = itemData.TypeId,
-                        MaterialId = itemData.MaterialId,
-                        MinimumAvailableQuantity = itemData.MinimumAvailableQuantity,
-                        Description = itemData.Description,
-                        IsActive = itemData.IsActive,
+                        ItemName = companyData.Name,
+                        Description = companyData.Description,
+                        IsActive = companyData.IsActive,
                         CreatedBy = userData.Id,
                         CreatedOn = DateTime.Now
                     });
@@ -76,14 +71,14 @@ namespace ClientManager.Controllers
                 if (num > 0)
                     data = new JsonReponse()
                     {
-                        message = "Item created successfully!",
+                        message = "Company created successfully!",
                         status = "Success",
-                        redirectURL = "/items/List"
+                        redirectURL = "/Company/List"
                     };
                 else
                     data = new JsonReponse()
                     {
-                        message = "Item creation not completed, try again after sometime.",
+                        message = "Company creation not completed, try again after sometime.",
                         status = "Failed",
                         redirectURL = ""
                     };
@@ -108,28 +103,27 @@ namespace ClientManager.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            DBOperation.Item expenceCategory = db.Items.Find(id);
-            if (expenceCategory == null)
+            DBOperation.Company company = db.Companies.Find(id);
+            if (company == null)
             {
                 return HttpNotFound();
             }
 
-            ViewBag.MaterialId = new SelectList(db.Materials, "MaterialId", "MaterialName", 1).ToList<SelectListItem>();
-            ViewBag.TypeId = new SelectList(db.Types, "TypeId", "TypeName", 1).ToList<SelectListItem>();
+           
             ViewBag.Status = new SelectList(Utility.DefaultList.GetStatusList(), "Value", "Text", (object)1).ToList<SelectListItem>();
-            return View(expenceCategory);
+            return View(company);
         }
 
         // POST: ExpenceCategories/Edit/5
         [HttpPost]
         [CustomAuthorize(new string[] { "Super Admin", "Super User" })]
-        public ActionResult Edit(Models.ItemData itemData)
+        public ActionResult Edit(Models.CompanyData companyData)
         {
             JsonReponse data;
             try
             {
                 UserDetails userDetails = (UserDetails)this.Session["UserDetails"];
-                DBOperation.Item entity = this.db.Items.FirstOrDefault(wh => wh.ItemId == itemData.ItemId);
+                DBOperation.Company entity = this.db.Companies.FirstOrDefault(wh => wh.CompanyId == companyData.CompanyId);
                 if (entity == null)
                     data = new JsonReponse()
                     {
@@ -137,7 +131,7 @@ namespace ClientManager.Controllers
                         status = "Failed",
                         redirectURL = ""
                     };
-                else if (string.IsNullOrEmpty(itemData.ItemName) || string.IsNullOrEmpty(itemData.Description))
+                else if (string.IsNullOrEmpty(companyData.Name) || string.IsNullOrEmpty(companyData.Description))
                 {
                     data = new JsonReponse()
                     {
@@ -148,17 +142,14 @@ namespace ClientManager.Controllers
                 }
                 else
                 {
-                    this.db.Entry<DBOperation.Item>(entity).State = EntityState.Modified;
+                    this.db.Entry<DBOperation.Company>(entity).State = EntityState.Modified;
                     string str;
                     if (userDetails.UserRoles.Any<ClientManager.Models.UserRole>((Func<ClientManager.Models.UserRole, bool>)(wh => wh.RoleName.ToLower() == "super admin")))
                     {
-                        entity.ItemName = itemData.ItemName;
-                        entity.TypeId = itemData.TypeId;
-                        entity.MaterialId = itemData.MaterialId;
-                        entity.Description = itemData.Description;
-                        entity.IsActive = itemData.IsActive;  
-                        entity.MinimumAvailableQuantity = itemData.MinimumAvailableQuantity;
-                        //str = "Item details Updated";
+                        entity.Name = companyData.Name;
+                        entity.Description = companyData.Description;
+                        entity.IsActive = companyData.IsActive;                       
+                        str = "Expence Category Updated";
                     }
 
                     entity.ModifiedBy = new int?(userDetails.Id);
@@ -167,7 +158,7 @@ namespace ClientManager.Controllers
                     if (this.db.SaveChanges() > 0)
                         data = new JsonReponse()
                         {
-                            message = "Item details Updated successfully!",
+                            message = "Company details updated successfully!",
                             status = "Success",
                             redirectURL = "/items/List"
                         };
@@ -200,7 +191,7 @@ namespace ClientManager.Controllers
             try
             {
 
-                DBOperation.Item entity = this.db.Items.FirstOrDefault(wh => wh.ItemId == id);
+                DBOperation.Company entity = this.db.Companies.FirstOrDefault(wh => wh.CompanyId == id);
 
 
                 if (entity == null)
@@ -214,7 +205,7 @@ namespace ClientManager.Controllers
                 }
                 else
                 {
-                    this.db.Entry<DBOperation.Item>(entity).State = EntityState.Modified;
+                    this.db.Entry<DBOperation.Company>(entity).State = EntityState.Modified;
                     entity.IsActive = true;
 
                     if (this.db.SaveChanges() > 0)
@@ -258,7 +249,7 @@ namespace ClientManager.Controllers
             try
             {
 
-                DBOperation.Item entity = this.db.Items.FirstOrDefault(wh => wh.ItemId == id);
+                DBOperation.Company entity = this.db.Companies.FirstOrDefault(wh => wh.CompanyId == id);
 
 
                 if (entity == null)
@@ -272,7 +263,7 @@ namespace ClientManager.Controllers
                 }
                 else
                 {
-                    this.db.Entry<DBOperation.Item>(entity).State = EntityState.Modified;
+                    this.db.Entry<DBOperation.Company>(entity).State = EntityState.Modified;
                     entity.IsActive = false;
 
                     if (this.db.SaveChanges() > 0)
