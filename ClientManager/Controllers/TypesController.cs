991b3b81b93cad07c1b0486977d2e4ceba7da36f
ravipoc <anbu.ravi04@outlook.com>
2022-09-29 00:37:12 +0530

    Chnages done

--- ClientManager/Controllers/TypesController.cs
+++ ClientManager/Controllers/TypesController.cs
@@ -12,7 +12,7 @@ using DBOperation;
 
 namespace ClientManager.Controllers
 {
-    public class ItemsController : Controller
+    public class TypesController : Controller
     {
         private ClientManagerEntities db = new ClientManagerEntities();
 
@@ -20,8 +20,8 @@ namespace ClientManager.Controllers
         [CustomAuthorize(new string[] { "Super Admin","Super User" })]
         public ActionResult List()
         {
-            var items = db.Items;
-            return View(items.ToList());
+            var types = db.Types;
+            return View(types.ToList());
         }
 
         // GET: ExpenceCategories/Create
@@ -29,7 +29,6 @@ namespace ClientManager.Controllers
         public ActionResult Create()
         {
             ViewBag.MaterialId = new SelectList(db.Materials, "MaterialId", "MaterialName", 1).ToList<SelectListItem>();
-            ViewBag.TypeId = new SelectList(db.Types, "TypeId", "TypeName", 1).ToList<SelectListItem>();
             ViewBag.Status = new SelectList(Utility.DefaultList.GetStatusList(), "Value", "Text", 1).ToList<SelectListItem>();
             return View();
         }
@@ -39,7 +38,7 @@ namespace ClientManager.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [CustomAuthorize(new string[] { "Super Admin", "Super User" })]
-        public ActionResult Create(Models.ItemData itemData)
+        public ActionResult Create(Models.TypesData typesData)
         {
             UserDetails userData = (UserDetails)this.Session["UserDetails"];
             
@@ -49,7 +48,7 @@ namespace ClientManager.Controllers
             try
             {
                 int num = 0;
-                if (string.IsNullOrEmpty(itemData.ItemName) || string.IsNullOrEmpty(itemData.Description))
+                if (string.IsNullOrEmpty(typesData.TypeName) || string.IsNullOrEmpty(typesData.Description))
                 {
                     jsonReponse = new JsonReponse()
                     {
@@ -60,14 +59,12 @@ namespace ClientManager.Controllers
                 }
                 else
                 {
-                    this.db.Items.Add(new DBOperation.Item()
+                    this.db.Types.Add(new DBOperation.Type()
                     {
-                        ItemName = itemData.ItemName,
-                        TypeId = itemData.TypeId,
-                        MaterialId = itemData.MaterialId,
-                        MinimumAvailableQuantity = itemData.MinimumAvailableQuantity,
-                        Description = itemData.Description,
-                        IsActive = itemData.IsActive,
+                        MaterialId = typesData.MaterialId,
+                        TypeName = typesData.TypeName,
+                        Description = typesData.Description,
+                        IsActive = typesData.IsActive,
                         CreatedBy = userData.Id,
                         CreatedOn = DateTime.Now
                     });
@@ -76,14 +73,14 @@ namespace ClientManager.Controllers
                 if (num > 0)
                     data = new JsonReponse()
                     {
-                        message = "Item created successfully!",
+                        message = "Type created successfully!",
                         status = "Success",
-                        redirectURL = "/items/List"
+                        redirectURL = "/Types/List"
                     };
                 else
                     data = new JsonReponse()
                     {
-                        message = "Item creation not completed, try again after sometime.",
+                        message = "Typr creation not completed, try again after sometime.",
                         status = "Failed",
                         redirectURL = ""
                     };
@@ -108,28 +105,27 @@ namespace ClientManager.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            DBOperation.Item expenceCategory = db.Items.Find(id);
-            if (expenceCategory == null)
+            DBOperation.Type typeData = db.Types.Find(id);
+            if (typeData == null)
             {
                 return HttpNotFound();
             }
 
-            ViewBag.MaterialId = new SelectList(db.Materials, "MaterialId", "MaterialName", 1).ToList<SelectListItem>();
-            ViewBag.TypeId = new SelectList(db.Types, "TypeId", "TypeName", 1).ToList<SelectListItem>();
+            ViewBag.MaterialId = new SelectList(db.Materials, "MaterialId", "MaterialName").ToList<SelectListItem>();
             ViewBag.Status = new SelectList(Utility.DefaultList.GetStatusList(), "Value", "Text", (object)1).ToList<SelectListItem>();
-            return View(expenceCategory);
+            return View(typeData);
         }
 
         // POST: ExpenceCategories/Edit/5
         [HttpPost]
         [CustomAuthorize(new string[] { "Super Admin", "Super User" })]
-        public ActionResult Edit(Models.ItemData itemData)
+        public ActionResult Edit(Models.TypesData typesData)
         {
             JsonReponse data;
             try
             {
                 UserDetails userDetails = (UserDetails)this.Session["UserDetails"];
-                DBOperation.Item entity = this.db.Items.FirstOrDefault(wh => wh.ItemId == itemData.ItemId);
+                DBOperation.Type entity = this.db.Types.FirstOrDefault(wh => wh.TypeId == typesData.TypeId);
                 if (entity == null)
                     data = new JsonReponse()
                     {
@@ -137,7 +133,7 @@ namespace ClientManager.Controllers
                         status = "Failed",
                         redirectURL = ""
                     };
-                else if (string.IsNullOrEmpty(itemData.ItemName) || string.IsNullOrEmpty(itemData.Description))
+                else if (string.IsNullOrEmpty(typesData.TypeName) || string.IsNullOrEmpty(typesData.Description))
                 {
                     data = new JsonReponse()
                     {
@@ -148,28 +144,26 @@ namespace ClientManager.Controllers
                 }
                 else
                 {
-                    this.db.Entry<DBOperation.Item>(entity).State = EntityState.Modified;
+                    this.db.Entry<DBOperation.Type>(entity).State = EntityState.Modified;
                     string str;
                     //if (userDetails.UserRoles.Any<ClientManager.Models.UserRole>((Func<ClientManager.Models.UserRole, bool>)(wh => wh.RoleName.ToLower() == "super admin")))
                     //{
-                        entity.ItemName = itemData.ItemName;
-                        entity.TypeId = itemData.TypeId;
-                        entity.MaterialId = itemData.MaterialId;
-                        entity.Description = itemData.Description;
-                        entity.IsActive = itemData.IsActive;  
-                        entity.MinimumAvailableQuantity = itemData.MinimumAvailableQuantity;
-                        //str = "Item details Updated";
+                        entity.MaterialId = typesData.MaterialId;
+                        entity.TypeName = typesData.TypeName;
+                        entity.Description = typesData.Description;
+                        entity.IsActive = typesData.IsActive;                       
+                        str = "Type details Updated";
                     //}
-
+                   
                     entity.ModifiedBy = new int?(userDetails.Id);
                     entity.ModifiedOn = new DateTime?(DateTime.Now);
 
                     if (this.db.SaveChanges() > 0)
                         data = new JsonReponse()
                         {
-                            message = "Item details Updated successfully!",
+                            message = "Type details saved successfully!",
                             status = "Success",
-                            redirectURL = "/items/List"
+                            redirectURL = "/Types/List"
                         };
                     else
                         data = new JsonReponse()
@@ -200,8 +194,8 @@ namespace ClientManager.Controllers
             try
             {
 
-                DBOperation.Item entity = this.db.Items.FirstOrDefault(wh => wh.ItemId == id);
-
+                DBOperation.Type entity = this.db.Types.FirstOrDefault(wh => wh.TypeId == id);
+                UserDetails userDetails = (UserDetails)this.Session["UserDetails"];
 
                 if (entity == null)
                 {
@@ -209,13 +203,15 @@ namespace ClientManager.Controllers
                     {
                         message = "There is no record for given Id",
                         status = "Failed",
-                        redirectURL = "/Items/List"
+                        redirectURL = "/Types/List"
                     };
                 }
                 else
                 {
-                    this.db.Entry<DBOperation.Item>(entity).State = EntityState.Modified;
+                    this.db.Entry<DBOperation.Type>(entity).State = EntityState.Modified;
                     entity.IsActive = true;
+                    entity.ModifiedBy = new int?(userDetails.Id);
+                    entity.ModifiedOn = new DateTime?(DateTime.Now);
 
                     if (this.db.SaveChanges() > 0)
                     {
@@ -223,7 +219,7 @@ namespace ClientManager.Controllers
                         {
                             message = "Activated Successfully!",
                             status = "Success",
-                            redirectURL = "/Items/List"
+                            redirectURL = "/Types/List"
                         };
                     }
                     else
@@ -232,7 +228,7 @@ namespace ClientManager.Controllers
                         {
                             message = "Failed to update!",
                             status = "Error",
-                            redirectURL = "/Items/List"
+                            redirectURL = "/Types/List"
                         };
                     }
 
@@ -244,7 +240,7 @@ namespace ClientManager.Controllers
                 {
                     message = ex.Message,
                     status = "Error",
-                    redirectURL = "/Items/List"
+                    redirectURL = "/Types/List"
                 };
             }
             return (ActionResult)this.Json((object)data, JsonRequestBehavior.AllowGet);
@@ -258,8 +254,8 @@ namespace ClientManager.Controllers
             try
             {
 
-                DBOperation.Item entity = this.db.Items.FirstOrDefault(wh => wh.ItemId == id);
-
+                DBOperation.Type entity = this.db.Types.FirstOrDefault(wh => wh.TypeId == id);
+                UserDetails userDetails = (UserDetails)this.Session["UserDetails"];
 
                 if (entity == null)
                 {
@@ -267,13 +263,15 @@ namespace ClientManager.Controllers
                     {
                         message = "There is no record for given Id",
                         status = "Failed",
-                        redirectURL = "/Items/List"
+                        redirectURL = "/Types/List"
                     };
                 }
                 else
                 {
-                    this.db.Entry<DBOperation.Item>(entity).State = EntityState.Modified;
+                    this.db.Entry<DBOperation.Type>(entity).State = EntityState.Modified;
                     entity.IsActive = false;
+                    entity.ModifiedBy = new int?(userDetails.Id);
+                    entity.ModifiedOn = new DateTime?(DateTime.Now);
 
                     if (this.db.SaveChanges() > 0)
                     {
@@ -281,7 +279,7 @@ namespace ClientManager.Controllers
                         {
                             message = "De-Activated Successfully!",
                             status = "Success",
-                            redirectURL = "/Items/List"
+                            redirectURL = "/Types/List"
                         };
                     }
                     else
@@ -290,7 +288,7 @@ namespace ClientManager.Controllers
                         {
                             message = "Failed to update!",
                             status = "Error",
-                            redirectURL = "/Items/List"
+                            redirectURL = "/Types/List"
                         };
                     }
 
@@ -307,7 +305,6 @@ namespace ClientManager.Controllers
             }
             return (ActionResult)this.Json((object)data, JsonRequestBehavior.AllowGet);
         }
-
         protected override void Dispose(bool disposing)
         {
             if (disposing)
